a92a62f9d9ba4a34fddb83df4586c25ef561b84c
Miharu-380 <38t.ajlkk@gmail.com>
2021-04-23 02:20:59 +0000

    [Add3]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '5de68f196bda9bd573dca76dd2250e0681e29775a16fee7562e17d6e720c22670be000f0722c5de8aa651fd9daf5ba5b8ba682278b6eb01f1a335c8235b4e0ee'
